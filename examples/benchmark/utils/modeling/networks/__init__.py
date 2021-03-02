bdaa3ec6b9694fd1021f032c1c46b8b214cd830c
starlyxxx <xinwang11@umbc.edu>
2021-03-01 22:14:15 -0500

    24000 random data

--- examples/benchmark/utils/modeling/networks/__init__.py
+++
@@ -1,4 +0,0 @@
-from utils.modeling.networks.classification import Classification
-from utils.modeling.networks.masked_lm import MaskedLM
-from utils.modeling.networks.span_labeling import SpanLabeling
-from utils.modeling.networks.transformer_encoder import TransformerEncoder
